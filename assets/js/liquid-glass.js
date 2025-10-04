5ec55a4d4d607ad3c7c7097853f91ea97085cb06
monozu-deving <monozu.deving@gmail.com>
2025-10-04 23:20:16 +0900

    glass v2.5

---
+++ assets/js/liquid-glass.js
@@ -0,0 +1,10 @@
+document.addEventListener("mousemove", (e) => {
+    document.querySelectorAll(".glass-btn").forEach((btn) => {
+      const rect = btn.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      btn.style.setProperty("--x", `${x}px`);
+      btn.style.setProperty("--y", `${y}px`);
+    });
+  });
+  
